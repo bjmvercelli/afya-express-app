57ad7de5b675bcb6baf248318cb6c6c3d7765296
Bruno Vercelli <bruno.vercelli@hotmail.com>
2021-05-21 19:58:25 -0300

    title change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Afya Labs - Express Web App' });
+  res.render('index', { title: 'Afya Labs - Bruno Vercelli' });
 });
 
 module.exports = router;
